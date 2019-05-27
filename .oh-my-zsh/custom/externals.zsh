86667736fac8203fbc30743411d1b45edc05a29c
Romain Commandé <commande.romain@gmail.com>
2019-05-27 17:53:46 +0200

    Update zsh configuration

--- .oh-my-zsh/custom/externals.zsh
+++ .oh-my-zsh/custom/externals.zsh
@@ -4,7 +4,12 @@
 source /usr/share/fzf/key-bindings.zsh
 
 #Powerline
-powerline-daemon -q
-source /usr/lib/python3.6/site-packages/powerline/bindings/zsh/powerline.zsh
+#powerline-daemon -q
+#source /usr/lib/python3.6/site-packages/powerline/bindings/zsh/powerline.zsh
 
-eval `keychain --eval --agents ssh id_rsa -q`
+eval `keychain --eval -q`
+#eval `keychain -q` 
+
+
+# added by travis gem
+#[ -f /home/romain/.travis/travis.sh ] && source /home/romain/.travis/travis.sh
