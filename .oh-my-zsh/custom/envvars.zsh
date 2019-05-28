415d83932452c121d022481ed5c075df0599b14e
Romain Commandé <rcommande@meilleursagents.com>
2019-05-28 19:14:21 +0200

    Fix TERM configuration

--- .oh-my-zsh/custom/envvars.zsh
+++ .oh-my-zsh/custom/envvars.zsh
@@ -1,7 +1,8 @@
 export EDITOR="nvim"
 export PATH="$HOME/.tfenv/bin:$HOME/.gem/ruby/2.5.0/bin/:"$PATH
 export XDG_CONFIG_HOME=$HOME'/.config'
-export TERM='tmux-256color'
+
+export TERM='xterm-256color'
 export COLORTERM='truecolor'
 export TERMINAL='kitty'
 
