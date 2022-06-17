63b0733d81d12e1e4af027dfb00e11b1736f86cc
Romain Commandé <romain.commande@wttj.co>
2022-06-17 18:08:37 +0200

    update

--- .oh-my-zsh/custom/envvars.zsh
+++ .oh-my-zsh/custom/envvars.zsh
@@ -2,7 +2,7 @@ export EDITOR="nvim"
 export PATH="$HOME/.yarn/bin/:$HOME/.tfenv/bin:$HOME/.gem/ruby/2.6.0/bin/:/home/romain/go/bin/:"$PATH
 export XDG_CONFIG_HOME=$HOME'/.config'
 
-export TERM='xterm-256color'
+export TERM='screen-256color'
 export COLORTERM='truecolor'
 export TERMINAL='alacritty'
 
