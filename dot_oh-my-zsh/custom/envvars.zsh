b7ff8683f242a7cab5a98aaee94c9ce9e105577d
Romain Commandé <romain.commande@wttj.co>
2023-11-10 11:50:46 +0100

    add oh-my-zsh-config

---
+++ dot_oh-my-zsh/custom/envvars.zsh
@@ -0,0 +1,16 @@
+export EDITOR="nvim"
+export PATH="$HOME/.yarn/bin/:$HOME/.tfenv/bin:$HOME/.gem/ruby/2.6.0/bin/:/home/romain/go/bin/:/home/romain/.asdf/installs/ruby/2.7.3/bin/:"$PATH
+export XDG_CONFIG_HOME=$HOME'/.config'
+
+export TERM='tmux-256color'
+export COLORTERM='truecolor'
+export TERMINAL='alacritty'
+
+unsetopt nomatch
+
+export DISABLE_AUTO_TITLE='true'
+
+export DOCKER_BUILDKIT=1
+export CYPRESS_NO_COMMAND_LOG=1
+
+export PROJECTS_PATH="$HOME/Projects"
---
+++ dot_oh-my-zsh/custom/externals.zsh
@@ -0,0 +1,41 @@
+#FZF
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+source /usr/share/fzf/key-bindings.zsh
+
+#Powerline
+#powerline-daemon -q
+#source /usr/lib/python3.6/site-packages/powerline/bindings/zsh/powerline.zsh
+
+eval `keychain --eval -q --agents "ssh,gpg"`
+#eval `keychain -q` 
+
+
+# added by travis gem
+#[ -f /home/romain/.travis/travis.sh ] && source /home/romain/.travis/travis.sh
+
+
+# Pew autocompletion
+source $(pew shell_config)
+
+# Tmuxinator
+# source /home/romain/.gem/ruby/2.6.0/gems/tmuxinator-1.1.3/completion/tmuxinator.zsh
+
+# Goto
+#source "$HOME/.oh-my-zsh/custom/externals/goto/goto.sh"
+
+# Z
+[[ -r "/usr/share/z/z.sh" ]] && source /usr/share/z/z.sh 
+
+
+# PipX
+autoload -U bashcompinit
+bashcompinit
+eval "$(register-python-argcomplete pipx)"
+
+# asdf
+source /opt/asdf-vm/asdf.sh
+
+function ssh_jump() {
+  ssh -A -J $1 -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" $2
+}
