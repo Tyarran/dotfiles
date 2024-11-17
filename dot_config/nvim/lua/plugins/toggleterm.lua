08397ea4ef03b26afa55662d4d1aa51a72ba2bf2
Romain Commandé <commande.romain@gmail.com>
2024-11-17 18:21:52 +0100

    update

--- dot_config/nvim/lua/plugins/toggleterm.lua
+++ dot_config/nvim/lua/plugins/toggleterm.lua
@@ -20,28 +20,41 @@ return {
         }
       },
       winbar = {
-        enable = true
+        enable = true,
+        name_formatter = function(term) --  term: Terminal
+          return term.name
+        end
       },
       -- open_mapping = [[<C-/>]],
       on_open = function(term)
-        vim.cmd("startinsert")
         vim.api.nvim_buf_set_keymap(term.bufnr, "n", "<S-h>", "<nop>", { noremap = true, silent = true })
         vim.api.nvim_buf_set_keymap(term.bufnr, "n", "<S-l>", "<nop>", { noremap = true, silent = true })
         vim.api.nvim_buf_set_keymap(term.bufnr, "n", "<A-h>", "<nop>", { noremap = true, silent = true })
         vim.api.nvim_buf_set_keymap(term.bufnr, "n", "<A-l>", "<nop>", { noremap = true, silent = true })
-      end,
-      on_close = function(_term)
-        vim.cmd("stopinsert")
-      end
-    })
 
-    vim.keymap.set("n", "<C-_>", ":ToggleTerm<CR>", { noremap = true, silent = true })
+        vim.cmd("set nu")
+        vim.cmd("startinsert")
+      end,
+      hide_numbers = false,
+      insert_mappings = true,
+      terminal_mappings = true,
+    })
+    --
+    vim.keymap.set("n", "<S-_>", ":ToggleTerm<CR>", { noremap = true, silent = false })
+    vim.keymap.set("n", "<S-/>", ":ToggleTerm<CR>", { noremap = true, silent = false })
+
+    vim.keymap.set("t", "<S-_>", "<C-\\><C-n>:ToggleTerm<CR>", { noremap = true, silent = true })
+    vim.keymap.set("t", "<S-/>", "<C-\\><C-n>:ToggleTerm<CR>", { noremap = true, silent = true })
+
+
+    vim.keymap.set("n", "<C-_>", ":ToggleTerm<CR>", { noremap = true, silent = false })
     vim.keymap.set("n", "<C-/>", ":ToggleTerm<CR>", { noremap = true, silent = true })
     vim.keymap.set("i", "<C-_>", "<ESC>:ToggleTerm<CR>", { noremap = true, silent = true })
     vim.keymap.set("i", "<C-/>", "<ESC>:ToggleTerm<CR>", { noremap = true, silent = true })
+
     vim.keymap.set("t", "<C-_>", "<C-\\><C-n>:ToggleTerm<CR>", { noremap = true, silent = true })
     vim.keymap.set("t", "<C-/>", "<C-\\><C-n>:ToggleTerm<CR>", { noremap = true, silent = true })
     vim.keymap.set("t", "<ESC><ESC>", "<C-\\><C-n>", { noremap = true, silent = true })
-    vim.keymap.set("t", "<ESC>", "<nop>", { noremap = true, silent = true })
+    -- vim.keymap.set("t", "<ESC>", "<nop>", { noremap = true, silent = true })
   end
 }
