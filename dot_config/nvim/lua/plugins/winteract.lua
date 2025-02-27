5e65294ce1393b1a669c67353ca5f7da695dc89b
Romain Commandé <romain.commande@pledg.co>
2025-02-27 16:57:54 +0100

    update

--- dot_config/nvim/lua/plugins/winteract.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "romgrk/winteract.vim",
-  -- cmd = "InteractiveWindow",
-  config = function()
-    vim.keymap.set("n", "<Leader>w", "<cmd>InteractiveWindow<cr>", { silent = true })
-  end
-}
